27111cf32fab2361763fb21a24f8e45148e00350
SparkySimp <ozturkkivilcimcemal@protonmail.com>
2023-05-07 08:20:58 +0300

    Added docs for polyn.h, declared the types Set and Function

--- app/src/main/headers/polyn.h
+++ app/src/main/headers/polyn.h
@@ -6,22 +6,56 @@
 
 namespace emel_lang {
     namespace data {
+
+        /// <summary>
+        /// Represents a coefficient in a term.
+        /// </summary>
         typedef struct __Coefficient {
+            /// <summary>
+            /// The coefficient.
+            /// </summary>
             double coefficient;
+            /// <summary>
+            /// The exponent.
+            /// </summary>
             unsigned int exponent;
         } Coefficient;
 
+        /// <summary>
+        /// Represents a term in a polynomial.
+        /// </summary>
         typedef struct __Term {
+            /// <summary>
+            /// The coefficients of the term.
+            /// </summary>
             std::vector<Coefficient> coefficients;
+            /// <summary>
+            /// The variable of the term.
+            /// </summary>
             std::optional<std::string> variable;
+            /// <summary>
+            /// Parses a term from a string.
+            /// </summary>
             struct __Term Parse(std::string source);
         } Term;
 
+        /// <summary>
+        /// Represents a polynomial.
+        /// </summary>
         class Polynomial {
             private:
                 std::vector<Term> m_terms;
             public:
+                /// <summary>
+                /// Constructs a polynomial from a list of terms.
+                /// <param name="terms">The terms of the polynomial.</param>
+                /// </summary>
                 Polynomial(std::vector<Term> terms): m_terms(terms) {};
+
+                /// <summary>
+                /// Parses a polynomial from a string.
+                /// <param name="source">The string to parse.</param>
+                /// </summary>
                 static Polynomial Parse(std::string source);
         };
     }
